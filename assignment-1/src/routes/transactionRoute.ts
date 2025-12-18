626410048a1f50fd81de49f4c89402450577863e
Naridz <naris2001@gmail.com>
2025-12-19 00:20:40 +0700

    create transaction routes

--- assignment-1/src/routes/transactionRoute.ts
+++ assignment-1/src/routes/transactionRoute.ts
@@ -3,11 +3,11 @@ import { createTransaction, deleteTransaction, getTransactionById, getTransactio
 
 const router = Router();
 
-router.post("/", createTransaction);
-router.get("/", getTransactions);
-router.get("/:id", getTransactionById);
+router.post("/", createTransaction)
+router.get("/", getTransactions)
+router.get("/:id", getTransactionById)
 router.put("/:id", updateTransaction)
-router.delete("/:id", deleteTransaction);
-router.patch("/:id/restore", restoreTransaction);
+router.delete("/:id", deleteTransaction)
+router.patch("/:id/restore", restoreTransaction)
 
 export default router;
