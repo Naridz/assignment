6c8bb2baf08aadc8391c6b55e7bece3b1c2738be
Naridz <naris2001@gmail.com>
2025-12-19 00:19:52 +0700

    create transaction schema

--- assignment-1/src/models/transaction.ts
+++ assignment-1/src/models/transaction.ts
@@ -1,7 +1,7 @@
 import { Document, model, Schema } from "mongoose";
 
 export interface TransactionType extends Document{
-    type:"income" | "expense",
+    type:"income" | "expense" ,
     amount:number,
     description:string,
     date:Date,
